fd78ea9731d8c3b256371af802a0ece8d6a35b60
Nicholas1811 <nicholassdl660@gmail.com>
2021-03-05 00:30:30 +0800

    left with adding the website projects, and redo footer.
    mobile images are done. left with responsiveness checking + website projects + footer.
    button and href colors remove blue.

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -7,8 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {}
-
+  toRouter(){
+    this.router.navigateByUrl('/')
+  }
 }
--- src/app/project-item/project-item.page.ts
+++ src/app/project-item/project-item.page.ts
@@ -11,7 +11,6 @@ export class ProjectItemPage implements OnInit {
   projectObject;
   hrefvar;
   slideperview;
-
   segmentModel = "brief";
   slideOptsOne = {
     initialSlide: 0,
@@ -21,8 +20,10 @@ export class ProjectItemPage implements OnInit {
     speed: 500,
     loop: true
   };
+ 
 
   constructor(private router: Router) {
+
 
     this.hrefvar = "/portfolio/project-item/" + localStorage.getItem("id");
     this.projectArray = [
@@ -48,7 +49,12 @@ export class ProjectItemPage implements OnInit {
         type: "Mobile Application",
         technologiesUsed: "<ul><li>HTML</li><li>SCSS</li><li>Typescript</li><li>Cordova</li><li>Ionic Framework</li><li>Cloud Firestore</li></ul>",
         achievedGrade: "I have achieved an A Grade.",
-        image: "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/port2.png?alt=media&token=958313db-8e2b-4a8c-8b73-73279d6e3fac"
+        image: ["https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/ieatifindimake1.png?alt=media&token=35344789-07bf-4f05-b9f7-4881450103ce",
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/ieatifindimake2.png?alt=media&token=acca5b79-22ce-43ee-bcf6-a1ee8df0d626",
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/ieatifindimake3.png?alt=media&token=4e2bf017-86af-45b5-b42b-9b4a5e3e3c1b",
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/ieatifindimake4.png?alt=media&token=a6476cd3-d14a-471f-afc4-69729560397e",
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/ieatifindimake5.png?alt=media&token=be43ea45-30b5-4286-bc4a-ee3d31572b34"
+      ]
       },
       {
         id: "EZBEATS",
@@ -57,7 +63,12 @@ export class ProjectItemPage implements OnInit {
         type: "Mobile Application",
         technologiesUsed: "<ul><li>Java</li><li>Android Studios</li></ul>",
         achievedGrade: "I have achieved an A Grade.",
-        image: "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/port3.png?alt=media&token=14a7937a-cab4-4b55-9551-d15461f1e36b"
+        image: ["https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/port1.png?alt=media&token=84c9c0fa-fb2f-4d5c-b96c-7a5c608534df",
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/OHSS.png?alt=media&token=e3f40a32-ce26-4898-98ff-db95dbc8c8d1",
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/OHSS1.png?alt=media&token=9d7e0b77-ac6f-4d1a-95cc-9d6d659ef120",
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/OHSS2.png?alt=media&token=be7b6810-3962-4219-8b89-7b65d98a4133",
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/OHSS3.png?alt=media&token=910a895f-5edb-4b51-9c58-6d5b4f8b0e00"
+      ]
       }, {
         id: "TradeX",
         name: "TradeX",
@@ -65,15 +76,26 @@ export class ProjectItemPage implements OnInit {
         type: "Web Application",
         technologiesUsed: "<ul><li>HTML</li><li>CSS</li><li>Flask</li><li>Javascript</li></ul>",
         achievedGrade: "As of 04/03/2020, the grade for this project has not been released.",
-        image: "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/obkd.png?alt=media&token=0c8ce181-2804-47ad-971c-10f62bda8faf"
+        image: ["https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/ieatifindimake1.png?alt=media&token=35344789-07bf-4f05-b9f7-4881450103ce",
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/ieatifindimake2.png?alt=media&token=acca5b79-22ce-43ee-bcf6-a1ee8df0d626",
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/ieatifindimake3.png?alt=media&token=4e2bf017-86af-45b5-b42b-9b4a5e3e3c1b",
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/ieatifindimake4.png?alt=media&token=a6476cd3-d14a-471f-afc4-69729560397e",
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/ieatifindimake5.png?alt=media&token=be43ea45-30b5-4286-bc4a-ee3d31572b34"
+      ]
       }, {
         id: "KReviews",
-        name: "ieatifindimake",
+        name: "KReviews",
         des: "KReviews is a web application developed to demostrate basic understanding of Web Development. It is a restaurant review website. Some of the functionalities include, basic CRUD of reviews and user information, integration with Google Maps API, searching capabilities, amongst others. Users can rate and place reviews on these restaurants. ",
         type: "Web Application",
         technologiesUsed: "<ul><li>HTML</li><li>CSS</li><li>Javascript</li><li>Node.js</li><li>MySQL</li></ul>",
         achievedGrade: "I have achieved an A Grade.",
-        image: "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/Untitled.png?alt=media&token=6c7fe3ea-f6f9-42bc-b364-83436b326492"
+        image: ["https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/port1.png?alt=media&token=84c9c0fa-fb2f-4d5c-b96c-7a5c608534df",
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/OHSS.png?alt=media&token=e3f40a32-ce26-4898-98ff-db95dbc8c8d1",
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/OHSS1.png?alt=media&token=9d7e0b77-ac6f-4d1a-95cc-9d6d659ef120",
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/OHSS2.png?alt=media&token=be7b6810-3962-4219-8b89-7b65d98a4133",
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/OHSS3.png?alt=media&token=910a895f-5edb-4b51-9c58-6d5b4f8b0e00",
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-73db1.appspot.com/o/OHSS4.png?alt=media&token=02975455-827d-42ac-83ab-04bcdc3d5ac3",
+      ]
       },
 
     ]
